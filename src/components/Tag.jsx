ceb01a8d41b2c0a13c35426b723d14293e0ccb33
Mustafa <mrcoder569.mustafa@gmail.com>
2025-11-19 23:46:43 +0500

    Add PropTypes to Tag.jsx and fix ESLint warnings

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import PropTypes from "prop-types";
 
 import "./Tag.css";
 
@@ -10,15 +11,30 @@ const Tag = ({ tagName, selectTag, selected }) => {
         React: { backgroundColor: "#4cdafc" },
         default: { backgroundColor: "#f9f9f9" },
     };
+
     return (
         <button
             type='button'
             className='tag'
             style={selected ? tagStyle[tagName] : tagStyle.default}
-            onClick={() => selectTag(tagName)}>
+            onClick={() => selectTag(tagName)}
+        >
             {tagName}
         </button>
     );
+};
+
+
+Tag.propTypes = {
+    tagName: PropTypes.string.isRequired,
+    selectTag: PropTypes.func,
+    selected: PropTypes.bool,
+};
+
+// Default props set karne ke liye
+Tag.defaultProps = {
+    selectTag: () => {},
+    selected: false,
 };
 
 export default Tag;
